3b5d9a0badecffba488c6b475f39ab6ed2a3a69e
winfred958 <winfred958@gmail.com>
2018-06-09 08:39:58 +0800

    - -

--- training-design-pattern/src/main/java/com/winfred/traning/designpattern/observer/Test.java
+++ training-design-pattern/src/main/java/com/winfred/traning/designpattern/observer/Test.java
@@ -15,7 +15,7 @@ public class Test {
 		Observer observer3 = new ObserverThr();
 
 		// 被观察者
-		ObservableImp observable = new ObservableImp();
+		ObservableImpl observable = new ObservableImpl();
 
 
 		observable.addObserver(observer1);
@@ -27,17 +27,7 @@ public class Test {
 		observable.doAction(new TestEntity("3"));
 		observable.doAction(new TestEntity("4"));
 		observable.doAction(new TestEntity("5"));
-		
-		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-		Calendar calendar = Calendar.getInstance();
-		int total = 60;
-		for (int i = 0; i < total; i++) {
-			calendar.add(Calendar.DAY_OF_MONTH, -1);
-			System.out.println(
-					"spark-submit       --class com.yamibuy.datamining.etl.ec.branch.SearchModel       --master yarn           --deploy-mode cluster           --driver-memory 6g       --num-executors 40       --executor-memory 2g       --executor-cores 4       --conf spark.sql.warehouse.dir=hdfs://yamicdh5/user/yamipro/spark/warehouse       --files /etc/hive/conf/hive-site.xml       --jars /opt/cm/share/cmf/lib/mysql-connector-java.jar     /home/yamipro/git_dir/bi-engine/data-etl/target/data-etl.jar "
-							+ simpleDateFormat.format(calendar.getTime()));
-		}
-		
+
 	}
 
 	static class TestEntity {
