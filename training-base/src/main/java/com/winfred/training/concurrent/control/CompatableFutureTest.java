0a68fd00c4f64b02063cb12276a5cf81b69ca824
winfred958 <winfred958@gmail.com>
2019-08-21 15:55:38 +0800

    feat: update

--- training-base/src/main/java/com/winfred/training/concurrent/control/CompatableFutureTest.java
+++ training-base/src/main/java/com/winfred/training/concurrent/control/CompatableFutureTest.java
@@ -6,9 +6,56 @@ public class CompatableFutureTest {
 
     public static void main(String[] args) {
         CompletableFuture<String> cf1 = CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(1000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(1);
             return "1";
         });
 
+        CompletableFuture<String> cf2 = CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(500L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(2);
+            return "2";
+        });
+
+        CompletableFuture<String> cf3 = CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(2000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(3);
+            return "3";
+        });
+
+
+        CompletableFuture<String> cf4 = CompletableFuture.supplyAsync(() -> {
+            System.out.println(4);
+            return "4";
+        });
+
+
+        CompletableFuture<String> cf5 = CompletableFuture.supplyAsync(() -> {
+            System.out.println(5);
+            return "5";
+        });
+
+        CompletableFuture<Void> allOf = CompletableFuture
+                .allOf(
+                        cf1,
+                        cf2,
+                        cf3,
+                        cf4,
+                        cf5
+                );
+        allOf.join();
 
 
     }
