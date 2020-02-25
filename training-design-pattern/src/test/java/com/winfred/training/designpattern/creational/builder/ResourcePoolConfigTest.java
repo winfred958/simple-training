598cfae9693d12a27eb52d75f88b65e0ce61e3ae
kevin_678 <kevin_678@126.com>
2020-02-25 16:04:18 +0800

    feat: update + builder design pattern

---
+++ training-design-pattern/src/test/java/com/winfred/training/designpattern/creational/builder/ResourcePoolConfigTest.java
@@ -0,0 +1,17 @@
+package com.winfred.training.designpattern.creational.builder;
+
+import org.junit.Test;
+
+public class ResourcePoolConfigTest {
+
+    @Test
+    public void testBuilder() {
+        ResourcePoolConfig resourcePoolConfig = new ResourcePoolConfig.Builder()
+                .setName("builder-test")
+                .setCoreSize(8)
+                .setMaxSize(16)
+                .build();
+
+        resourcePoolConfig.getCoreSize();
+    }
+}
