2e05ce06fc05ad78cf720e1b9b4f2c97ed4046bd
kevin_789 <kevin_789@126.com>
2020-07-08 17:28:42 +0800

    feat: update

--- training-elasticsearch/src/main/java/com/winfred/elastic/config/ElasticsearchConfig.java
+++ training-elasticsearch/src/main/java/com/winfred/elastic/config/ElasticsearchConfig.java
@@ -5,17 +5,45 @@ import org.apache.http.message.BasicHeader;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
-public class ElasticsearchConfig {
+public class ElasticsearchConfig implements FactoryBean<RestHighLevelClient>, InitializingBean, DisposableBean {
   
-  @Bean
-  public RestHighLevelClient getElasticClient() {
+  private RestHighLevelClient restHighLevelClient;
+  
+  @Override
+  public void destroy() throws Exception {
+    restHighLevelClient.close();
+  }
+  
+  @Override
+  public RestHighLevelClient getObject() throws Exception {
+    return restHighLevelClient;
+  }
+  
+  @Override
+  public Class<?> getObjectType() {
+    return RestHighLevelClient.class;
+  }
+  
+  @Override
+  public void afterPropertiesSet() throws Exception {
+    restHighLevelClient = getElasticClient();
+  }
+  
+  @Override
+  public boolean isSingleton() {
+    return false;
+  }
+  
+  private RestHighLevelClient getElasticClient() {
     RestClientBuilder restClientBuilder = RestClient.builder(
         new HttpHost("es-8gp5f0ej.public.tencentelasticsearch.com", 9200, "https")
     );
