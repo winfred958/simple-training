035de1059328848eb8a7139816ffffd23259e628
winfred958 <winfred958@gmail.com>
2021-02-22 11:16:06 +0800

    feat: update

--- training-base/src/main/java/com/winfred/training/juc/concurrent/simple/CyclicBarrierTest.java
+++ training-base/src/main/java/com/winfred/training/juc/concurrent/simple/CyclicBarrierTest.java
@@ -7,11 +7,16 @@ import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 
 /**
+ * 大家一起出发, 到达某个集合点, 然后大家再一起出发去下一个集合点
+ *
  * @author winfred958
  */
 @Slf4j
 public class CyclicBarrierTest {
 
+  /**
+   * 可重复使用
+   */
   private static CyclicBarrier cyclicBarrier = new CyclicBarrier(5);
 
   public static void main(String[] args) {
@@ -34,16 +39,16 @@ public class CyclicBarrierTest {
     public void run() {
       final int parties = cyclicBarrier.getParties();
       for (int i = 0; i < parties; i++) {
-        int randomNum = (int) (Math.random() * 1000);
+        int randomNum = (int) (Math.random() * 1000) + 1000;
         try {
           Thread.sleep(randomNum);
-          log.info("{} 通过障碍物: {}", number, i);
+          log.info("队员{} 到达集合点: {}", number, i);
+          // 等待其他线程
           cyclicBarrier.await();
         } catch (InterruptedException | BrokenBarrierException e) {
           log.error("", e);
         }
       }
-
     }
   }
 }
